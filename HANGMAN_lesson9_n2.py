e47a6459a83c8edfc0c52efd57080c43f5b962dc
GumpyGreed <75175487+GumpyGreed@users.noreply.github.com>
2020-12-27 09:59:09 +0300

    Add files via upload

---
+++ HANGMAN_lesson9_n2.py
@@ -0,0 +1,47 @@
+from random import randint
+
+def availability(w):
+      if w in user_word:
+            return True
+      else:
+            return False
+def word_order(w):
+      index = user_word.index(w, 0, (len(user_word)+1))
+      return index
+def user_gap(w):
+      quantity = len(w)
+      user_answer = []
+      for i in range(quantity):
+             user_answer.append('_')
+      return user_answer
+def appender (w, i):
+      delited = user_answer.pop(i)
+      user_answer.insert(i, w)
+      return user_answer
+
+def random_word ():
+    word_list = ['orange', 'melon', 'table', 'forge', 'thunder']
+    word = word_list[randint(0, 5)]
+    return word
+
+        
+user_word = str(random_word())
+user_word = list(user_word)
+counter = 0
+misstake = 3
+user_answer = user_gap(user_word)
+while counter < misstake:
+      user_input = str(input())
+      if availability(user_input):
+           index = word_order(user_input)
+           user_answer = appender(user_input, index)
+           print (user_answer)
+           if user_answer == user_word:
+                 print('Win! :)')
+                 break
+      else:
+            print('Misstake!')
+            counter += 1
+
+if counter == misstake:
+  print('Lose! :(')
